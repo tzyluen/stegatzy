085c862ac10939e578a7583a4d99f92a8f29c20d
Ng, Tzy Luen <tzyluen.ng@gmail.com>
2014-06-08 00:39:31 +0800

    added stegatzy.c (main)

---
+++ stegatzy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "tzybitmap.h"
+
+int main(int argc, char **argv)
+{
+    t_bitmap *bmp = create_bitmap(150);
+
+    return EXIT_SUCCESS;
+}
