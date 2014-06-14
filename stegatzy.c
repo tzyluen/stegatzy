67cfd1f70ba2868ff5322c9f53bd8aa62a39cf99
Ng, Tzy Luen <tzyluen.ng@gmail.com>
2014-06-14 19:42:04 +0800

    Added three new functions for reading bitmap files
    Those functions are,
    ./tzybitmap.h:void read_bitmap_file_header(t_bitmap *, FILE *);
    ./tzybitmap.h:void read_bitmap_info_header(t_bitmap *, FILE *);
    ./tzybitmap.h:int read_bitmap_file(const char *, t_bitmap *);

--- stegatzy.c
+++ stegatzy.c
@@ -6,6 +6,12 @@
 int main(int argc, char **argv)
 {
     t_bitmap *bmp = create_bitmap(150);
+    set_bitmap(bmp, 1);
+    unset_bitmap(bmp, 1);
+    int i = get_bitmap(bmp, 1);
+    printf("get_bitmap(bmp, 1): %d\n", i);
+    int ret = read_bitmap_file("./brucelee.bmp", bmp);
+    printf("read_bitmap_file: %d\n", ret);
 
     return EXIT_SUCCESS;
 }
--- tzybitmap.c
+++ tzybitmap.c
@@ -4,25 +4,54 @@
 #include "tzybitmap.h"
 
 
-void set_bitmap(t_bitmap bmp, int i)
+void set_bitmap(t_bitmap *bmp, int i)
 {
     printf("set_bitmap\n");
 }
 
 
-void unset_bitmap(t_bitmap bmp, int i)
+void unset_bitmap(t_bitmap *bmp, int i)
 {
     printf("unset_bitmap\n");
 }
 
 
-int get_bitmap(t_bitmap bmp, int i)
+int get_bitmap(t_bitmap *bmp, int i)
 {
-    return 0;
+    return i;
 }
 
 
 t_bitmap *create_bitmap(int n)
 {
     return (t_bitmap *)malloc((n+7)/8);
+}
+
+
+int read_bitmap_file(const char *file, t_bitmap *bmp)
+{
+    int error = 0;
+    FILE *bmpfile = fopen(file, "rb");
+    read_bitmap_file_header(bmp, bmpfile);
+    read_bitmap_info_header(bmp, bmpfile);
+
+    return error;
+}
+
+
+void read_bitmap_file_header(t_bitmap *bmp, FILE *img)
+{
+    //size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
+    fread(&bmp->header, sizeof(unsigned char), sizeof(t_BITMAPFILEHEADER), img);
+}
+
+
+void read_bitmap_info_header(t_bitmap *bmp, FILE *img)
+{
+    fread(&bmp->info_header, sizeof(unsigned char), sizeof(t_BITMAPINFOHEADER), img);
+}
+
+
+void streamout_bitmap(t_bitmap *bmp)
+{
 }
